8d671e82e1048ff7288733e1d36929840457ecfd
Egor Egorov <egigoka@gmail.com>
2022-08-29 21:41:51 +0600

    added dummy view for 'api/start_pomodoro'

--- pomodoro_everywhere_server/api/views.py
+++ pomodoro_everywhere_server/api/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.html. " \
                         + "We're not yet developed web application. Only API for native apps. " \
                         + "They're also not ready yet.")
+
+
+def start_pomodoro(request):
+    return HttpResponse("Not implemented")
