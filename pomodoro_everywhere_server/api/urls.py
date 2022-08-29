86d8ef1cfefa84e72af0ba44a488cd0b77cefa4f
Egor Egorov <egigoka@gmail.com>
2022-08-29 21:41:19 +0600

    added url for api call 'api/start_pomodoro'

--- pomodoro_everywhere_server/api/urls.py
+++ pomodoro_everywhere_server/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("start_pomodoro", views.start_pomodoro, name="start pomodoro api call")
 ]
