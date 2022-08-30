481e1d176170c8b0aa707edb7d69df11a48e3330
Egor Egorov <egigoka@gmail.com>
2022-08-30 16:50:19 +0600

    added some models

--- pomodoro_everywhere_server/api/models.py
+++ pomodoro_everywhere_server/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class PomodoroStatus(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    length_work = models.IntegerField(default=1200)
+    length_break = models.IntegerField(default=300)
+    length_long_break = models.IntegerField(default=900)
+    long_break_after = models.IntegerField(default=4)
+
+
+class PomodoroStatusLog(models.Model):
+    id = models.UUIDField
+    length_work = models.IntegerField(default=1200)
+    length_break = models.IntegerField(default=300)
+    length_long_break = models.IntegerField(default=900)
+    long_break_after = models.IntegerField(default=4)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    time = models.DateTimeField("Time")
+    status = models.ForeignKey(PomodoroStatus, on_delete=models.CASCADE)
